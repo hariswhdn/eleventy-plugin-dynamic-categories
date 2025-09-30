4ce4792a071c53519c15ac971919e44e7a638606
Haris Wahidin <hrswhdn@gmail.com>
2025-10-01 06:06:55 +0700

    fix uncategorized

--- src/getCategoryKeys.js
+++ src/getCategoryKeys.js
@@ -1,4 +1,12 @@
 module.exports = function (posts, options = {}) {
   const tagSet = new Set(posts.flatMap((post) => post.data[options.categoryVar] || []))
-  return [...tagSet]
+  const hasUncategorizedPosts = posts.some((post) => {
+    const categories = post.data[options.categoryVar]
+    return !Array.isArray(categories) || categories.length === 0
+  })
+  const categories = [...tagSet]
+  if (hasUncategorizedPosts) {
+    categories.push('Uncategorized')
+  }
+  return categories
 }
